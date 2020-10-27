05b06af0cc0e18dca77411ee0d637b9b7512b46e
Joao Pedro <802.018@alunos.unigran.br>
2020-10-27 16:51:42 -0300

    Token com jwt

--- APIDEGAMES/index.js
+++ APIDEGAMES/index.js
@@ -2,15 +2,47 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret ="sakdmaskmdkasdlasdkm";
 
 app.use(cors());
-
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //esse comando serve para evitar que quere a api quando requisitado. (já utilizado) 
 //npm install cors --save
+
+function auth(req,res,next){
+    const authToken = req.headers['authorization'];
+    
+    if(authToken != undefined){
+        const bearer = authToken.split('');
+      
+
+        jwt.verify(token,JWTSecret,(err,data)=>{
+            if(err){
+                res.status(401);
+                res.json({err:"Token invalido"});
+            }else{
+                req.token = token;
+                req.loggerUser = {id:data.id,email:data.email};
+                req.empresa= "jooj Interprice";
+                // console.log(data);
+                next();
+            }
+        });
+
+    }else{
+        res.status(401);
+        res.json({err:"Token invalido"});
+    }
+  
+    next();
+}
+
+
+
 
 // Fake Database
 var DB = {
@@ -37,6 +69,20 @@ var DB = {
         }
        
 
+    ],
+    users:[
+        {
+            id:1,
+            name: "jooj",
+            email:"jooj@gmail.com",
+            password: "jojo"
+        },
+        {
+            id:20,
+            name: "peep",
+            email: "peep@gmail.com",
+            password:"pepe"
+        }
     ]
 
 
@@ -44,12 +90,13 @@ var DB = {
 
 }
 //busca todos os dados
-app.get("/games",(req,res)=>{
+app.get("/games",auth,(req,res)=>{
+
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({empresa: req.empresa,user: req.loggerUser, games: DB.games});
 });
 //busca os dados individualmente pelo id
-app.get("/game/:id",(req,res)=>{
+app.get("/game/:id",auth,(req,res)=>{
 
     if(isNaN(req.params.id)){
         res.sendStatus(400);
@@ -68,7 +115,7 @@ app.get("/game/:id",(req,res)=>{
 });
 
 // Salvar mais dados
-app.post("/game",(req,res)=>{
+app.post("/game",auth,(req,res)=>{
 
         var {title,price,year} = req.body;
 
@@ -84,7 +131,7 @@ app.post("/game",(req,res)=>{
 });
 
 //deletar
-app.delete("/game/:id",(req,res)=>{
+app.delete("/game/:id",auth,(req,res)=>{
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
@@ -135,6 +182,46 @@ app.put("/game/:id",(req,res)=>{
 
 });
 
+
+app.post("/auth",(req,res)=>{
+    var {email,password} =req.body;
+
+    if(email !=undefined){
+      var user = DB.users.find(u=>u.email == email);
+    
+        if(user != undefined){
+
+            if(user.password == password){
+
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err,token)=>{
+                    if(err){
+                        
+                res.status(400);
+                res.json({token: "Falha Interna"});
+                    }else{
+                        
+                res.status(200);
+                res.json({token: token});
+                    }
+                });
+
+            }else{
+                res.status(200);   
+                res.json({err:"credenciais invalidas"});
+        
+            }
+
+        }else{
+            res.status(404);
+            res.json({err:"O E-mail não existe na base de dados"});
+        }
+    
+    
+    }else{  
+            res.status(400);
+            res.json({err:"O E-mail é invalido"});
+    }
+});
 
 app.listen(45678,()=>{
     console.log("API RODANDO");
