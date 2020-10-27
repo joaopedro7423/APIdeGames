9833b1d3ff28583677476d1a5ba7092600c99e7d
Joao Pedro <802.018@alunos.unigran.br>
2020-10-27 13:50:40 -0300

    NEW, MOD, DEL API final

--- index.js
+++ index.js
@@ -37,10 +37,96 @@ var DB = {
 
 
 }
-
+//busca todos os dados
 app.get("/games",(req,res)=>{
     res.statusCode = 200;
-   res.json(DB.games);
+    res.json(DB.games);
+});
+//busca os dados individualmente pelo id
+app.get("/game/:id",(req,res)=>{
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+            var id = parseInt(req.params.id);
+
+            var game = DB.games.find(g=>g.id==id);
+            if(game != undefined){
+                res.statusCode = 200;
+                res.json(game);
+            }else{
+                res.sendStatus(404);
+            }
+    }
+
+});
+
+// Salvar mais dados
+app.post("/game",(req,res)=>{
+
+        var {title,price,year} = req.body;
+
+        DB.games.push({
+            id: 2121,
+            title,
+            price,
+            year
+        });
+
+        res.sendStatus(200);
+
+});
+
+//deletar
+app.delete("/game/:id",(req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+            var id = parseInt(req.params.id);
+
+            var index = DB.games.findIndex(g=>g.id==id);
+       
+            if(index == -1){
+                res.sendStatus(404);
+            }else{
+                DB.games.splice(index,1);
+                res.sendStatus(200);
+            }
+    }
+    
+
+});
+
+// alterar 
+app.put("/game/:id",(req,res)=>{
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+            var id = parseInt(req.params.id);
+
+            var game = DB.games.find(g=>g.id==id);
+            if(game != undefined){
+            var {title,price,year} = req.body;
+
+                if(title != undefined){
+                        game.title = title;
+                }
+
+                if(price != undefined){
+                    game.price = price;
+                }
+
+                if(year != undefined){
+                    game.year = year;
+                }
+
+            }else{
+                res.sendStatus(404);
+            }
+    }
+      
+
 });
 
 
